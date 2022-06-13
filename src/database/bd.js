b06f1e0abdf5665eb3479b6eb49bbffc5f1ac47d
Lucas Viccari <lviccari4@gmail.com>
2022-06-13 20:25:45 -0300

    corrigindo pack Json

--- src/database/bd.js
+++ src/database/bd.js
@@ -3,14 +3,14 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
     process.env.DATABASE_URL,
     {
-        
+
         dialect: "postgres",
         dialectOptions: {
-            ssl:{
+            ssl: {
                 require: true,
                 rejectUnauthorized: false,
             }
         }
-    
-});
+
+    });
 module.exports = { sequelize };
